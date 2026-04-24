376e967e0af44d73dc040e46f312d6cfe6e245de
Cuervo <vidalcuervo@gmail.com>
2026-04-24 15:51:04 -0600

    videos 49-50

---
+++ Parte_4_Capitulos_41_65/video_49_Constantes/src/main/java/com/laboratorio/Main.java
@@ -0,0 +1,11 @@
+package com.laboratorio;
+
+public class Main {
+    public static void main(String[] args) {
+        Jugador jugador1;
+
+        jugador1 = new Jugador("Pedro");
+        jugador1.setNivel(3);
+        jugador1.mostrarJugador();
+    }
+}
---
+++ Parte_4_Capitulos_41_65/video_50_Constantes/src/main/java/com/laboratorio/Hija1.java
@@ -0,0 +1,24 @@
+package com.laboratorio;
+
+public class Hija1 {
+    String argumento;
+
+    public Hija1(String argumento) {
+
+        this.argumento = argumento;
+
+    }
+
+    public String getArgumento() {
+        return argumento;
+    }
+
+    public void setArgumento(String argumento) {
+        this.argumento = argumento;
+    }
+
+
+    public void mostrar() {
+        System.out.println("El Argumento es: " + argumento);
+    }
+}
