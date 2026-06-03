c10b15bbc4cbb7c2d4f25f5f33a97ccf820765d1
Cuervo <vidalcuervo@gmail.com>
2026-06-03 15:41:01 -0600

    video 92

---
+++ Parte_5_Capitulos_66_99/video_92_Consumir_API_REST_1/src/main/java/com/laboratorio/Main.java
@@ -0,0 +1,38 @@
+package com.laboratorio;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try{
+            //        Realizar la peticion a la API
+            URL url = new URL("https://api.frankfurter.dev/v1/latest");
+            HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+            conexion.setRequestMethod("GET");
+            conexion.connect();
+
+            //        Comprobar si la respuesta es correcta
+            int respuesta = conexion.getResponseCode();
+            //        Si no es correcta, mostramos mensaje de error
+            if (respuesta != 200) {
+                System.out.println("Ha ocurrido el erro: "+respuesta+
+                        " Procesando la petición ");
+                return;
+            }
+            //        Si es correcta, mostramos la respuesta
+            StringBuilder respuestaStr = new StringBuilder();
+            Scanner scanner = new Scanner(url.openStream());
+            while(scanner.hasNext()){
+                respuestaStr.append(scanner.nextLine());
+            }
+            scanner.close();
+            System.out.println("Respuesta: ");
+            System.out.println(respuestaStr);
+        } catch (Exception e) {
+            System.out.println("Error Inesperado al prosesar la petición:" +
+                    " " + e.getMessage());
+        }    }
+}
