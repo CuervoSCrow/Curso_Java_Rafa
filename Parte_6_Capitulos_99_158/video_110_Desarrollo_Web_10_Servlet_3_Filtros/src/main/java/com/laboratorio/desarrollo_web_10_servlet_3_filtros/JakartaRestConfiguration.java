7455dbce363b39153c7d96f064bccfed53b1842c
Cuervo <vidalcuervo@gmail.com>
2026-06-14 18:16:24 -0600

    video 110

---
+++ Parte_6_Capitulos_99_158/video_110_Desarrollo_Web_10_Servlet_3_Filtros/src/main/java/com/laboratorio/desarrollo_web_10_servlet_3_filtros/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.laboratorio.desarrollo_web_10_servlet_3_filtros;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ Parte_6_Capitulos_99_158/video_110_Desarrollo_Web_10_Servlet_3_Filtros/src/main/java/com/laboratorio/desarrollo_web_10_servlet_3_filtros/LogueadoFilter.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.laboratorio.desarrollo_web_10_servlet_3_filtros;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(".do")
+public class LogueadoFilter implements Filter{
+
+    private FilterConfig filterConfig=null;
+    
+    @Override
+    public void init(FilterConfig filterConfig){
+        this.filterConfig=filterConfig;
+    }
+    @Override
+    public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+        
+        log("He entrado al filtro LogueadoFilter");
+        
+        HttpServletRequest request =(HttpServletRequest) sr;
+        String usuario=(String) request.getSession().getAttribute("usuario");
+        
+        log("Valor del usuario: "+usuario);
+        
+        if((usuario!=null)&&(!usuario.isEmpty())){
+            fc.doFilter(sr, sr1);
+        }else{
+            HttpServletResponse response = (HttpServletResponse)sr1;
+            response.sendRedirect("index.jsp?error=Debe estar logueado para ver este contenido");
+        }
+    }
+    
+    public void log(String msg){
+        filterConfig.getServletContext().log(msg);
+    }
+}
