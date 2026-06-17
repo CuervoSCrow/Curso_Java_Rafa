d87fcfd50265862d8376af535eb5aee7337734fd
Cuervo <vidalcuervo@gmail.com>
2026-06-17 09:06:05 -0600

    video 110

---
+++ Parte_6_Capitulos_99_158/video_110_Desarrollo_Web_10_Servlet_3_Filtros_Ejercicios/video_110_Desarrollo_Web_11_Modelo_Vita_Controlador_MVC/src/main/java/com/laboratorio/video_110_desarrollo_web_11_modelo_vita_controlador_mvc/resources/JakartaEE11Resource.java
@@ -0,0 +1,20 @@
+package com.laboratorio.video_110_desarrollo_web_11_modelo_vita_controlador_mvc.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee11")
+public class JakartaEE11Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
