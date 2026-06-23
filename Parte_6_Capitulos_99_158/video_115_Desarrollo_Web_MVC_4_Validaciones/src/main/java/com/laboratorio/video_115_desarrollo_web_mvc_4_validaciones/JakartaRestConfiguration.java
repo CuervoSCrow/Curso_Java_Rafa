48f544aac25681826975e1b811c94a3046e9afc5
Cuervo <vidalcuervo@gmail.com>
2026-06-22 21:20:34 -0600

    video 115

---
+++ Parte_6_Capitulos_99_158/video_115_Desarrollo_Web_MVC_4_Validaciones/src/main/java/com/laboratorio/video_115_desarrollo_web_mvc_4_validaciones/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.laboratorio.video_115_desarrollo_web_mvc_4_validaciones;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
