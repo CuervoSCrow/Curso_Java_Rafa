661d7b52726091cd56a4dd3790b98ce04cd57582
Cuervo <vidalcuervo@gmail.com>
2026-07-01 09:38:22 -0600

    video 122

---
+++ Parte_6_Capitulos_99_158/video_122_Desarrollo_Web_23_Web_Service_4_WSRestful/src/main/java/com/laboratorio/video_122_desarrollo_web_23_web_service_4_wsrestful/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.laboratorio.video_122_desarrollo_web_23_web_service_4_wsrestful;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ Parte_6_Capitulos_99_158/video_122_Desarrollo_Web_23_Web_Service_4_WSRestful/src/main/java/com/laboratorio/video_122_desarrollo_web_23_web_service_4_wsrestful/resources/JakartaEE11Resource.java
@@ -0,0 +1,20 @@
+package com.laboratorio.video_122_desarrollo_web_23_web_service_4_wsrestful.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee11")
+public class JakartaEE11Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
